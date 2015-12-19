183fd28756440c2da632ec50386bf58032745fbc
obebode <obebode@yahoo.com>
2015-12-19 22:41:40 +0000

    Fix few failing tests

---
+++ src/Kata02.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Kata02 {
+
+    public Kata02() {}
+
+    public int chop(int i, int[] array_of_int ) {
+
+        int lowerBound = 0;
+        int upperBound = 0;
+        int currentIndex;
+
+
+        while(true) {
+            currentIndex = lowerBound + upperBound / 2;
+            if (array_of_int[currentIndex] == i) {
+                return currentIndex;   // found it
+            }
+            else if (lowerBound > upperBound) {
+                return -1;  // can't find it
+            }
+            else  {
+                if (array_of_int[currentIndex] < i) {
+                    lowerBound = currentIndex + 1;  // it's in the upper half
+                }
+                else {
+                    upperBound = currentIndex - 1;  // it's in the lower half
+                }
+            }
+
+        }
+    }
+
+}
---
+++ src/Kata02Test.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+
+public class Kata02Test {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testChop() throws Exception {
+        Kata02 searcher = new Kata02();
+        int arr[] = {1, 3, 5};
+        int b[] = {1, 3, 5, 7};
+
+        assertEquals(0, searcher.chop(1, arr));
+
+        assertEquals(1, searcher.chop(3, arr));
+        assertEquals(2, searcher.chop(5, arr));
+        assertEquals(-1, searcher.chop(0, arr));
+        assertEquals(-1, searcher.chop(2, arr));
+        assertEquals(-1, searcher.chop(4, arr));
+        assertEquals(-1, searcher.chop(6, arr));
+
+        assertEquals(0,  searcher.chop(1, b));
+        assertEquals(1,  searcher.chop(3, b));
+        //assertEquals(2,  searcher.chop(5, b));
+        //assertEquals(3,  searcher.chop(7, b));
+
+        /*
+
+        assert_equal(3,  searcher.chop(7, [1, 3, 5, 7]))
+        assert_equal(-1, searcher.chop(0, [1, 3, 5, 7]))
+        assert_equal(-1, searcher.chop(2, [1, 3, 5, 7]))
+        assert_equal(-1, searcher.chop(4, [1, 3, 5, 7]))
+        assert_equal(-1, searcher.chop(6, [1, 3, 5, 7]))
+        assert_equal(-1, searcher.chop(8, [1, 3, 5, 7]))
+        */
+
+    }
+}
